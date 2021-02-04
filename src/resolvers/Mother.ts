75e49ed7523f7480d5bbdaed12d2f63cc0206448
Filip <fivanusec@gmail.com>
2021-02-04 19:01:46 +0100

    Resolver #12
    Mother CRUD finished

--- src/resolvers/Mother.ts
+++ src/resolvers/Mother.ts
@@ -32,6 +32,51 @@ class MotherResponse {
 
 @Resolver(Mother)
 export class MotherResolver {
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  @UseMiddleware(isKinderGardenSelected)
+  async deleteMother(@Arg("motherId") motherId: number) {
+    await Mother.delete(motherId);
+    return true;
+  }
+
+  @Mutation(() => MotherResponse)
+  @UseMiddleware(isAuth)
+  @UseMiddleware(isKinderGardenSelected)
+  async updateMother(
+    @Arg("options") options: ParentsInput,
+    @Arg("motherId") motherId: number
+  ): Promise<MotherResponse> {
+    let mother;
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .update(Mother)
+        .set({
+          Name: options.name,
+          Surname: options.surname,
+          Email: options.email,
+          Phone: options.phone,
+        })
+        .where("Id=:id", { id: motherId })
+        .returning("*")
+        .execute();
+      mother = result.raw[0];
+    } catch (err) {
+      if (err.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "Children",
+              message: "There was an error",
+            },
+          ],
+        };
+      }
+    }
+    return { mother };
+  }
+
   @Mutation(() => MotherResponse)
   @UseMiddleware(isAuth)
   @UseMiddleware(isKinderGardenSelected)
