3a82b997384913655b6b5301317def5af80e69bc
Filip <fivanusec@gmail.com>
2021-02-09 10:11:46 +0100

    Frontend integrtation #2

---
+++ web/src/components/Wrapper.tsx
@@ -0,0 +1,24 @@
+import react from "react";
+import { Box } from "@chakra-ui/core";
+
+export type WrapperVariant = "small" | "regular" | "big";
+
+interface WrapperProps {
+  variant?: WrapperVariant;
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Box
+      m={8}
+      mx="auto"
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
+};
