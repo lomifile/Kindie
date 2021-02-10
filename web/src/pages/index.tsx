ebbecbb2c7210e7587ddef109a06c5deebac3ab6
Filip <fivanusec@gmail.com>
2021-02-10 19:14:17 +0100

    Frontend #1
    Basic layout,
    Navbar design

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../components/Layout";
+import { Wrapper } from "../components/Wrapper";
+
+const Index = () => {
+  return (
+    <Layout>
+      <div>Hello!</div>
+    </Layout>
+  );
+};
+
+export default Index;
