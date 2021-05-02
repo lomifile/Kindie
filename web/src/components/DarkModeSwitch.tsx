1c196d5f6ff1c1a907952c464a63d848b41a0985
Filip <fivanusec@gmail.com>
2021-05-02 17:01:53 +0200

    Dark mode #2

---
+++ web/src/components/DarkModeSwitch.tsx
@@ -0,0 +1,17 @@
+import { Switch, useColorMode } from "@chakra-ui/react";
+import React from "react";
+
+export const DarkModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+  return (
+    <Switch
+      position="fixed"
+      top="1rem"
+      right="1rem"
+      color="blue.100"
+      isChecked={isDark}
+      onChange={toggleColorMode}
+    />
+  );
+};
