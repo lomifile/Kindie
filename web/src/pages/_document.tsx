0bd0dba9290a5154f08adcdb587ed97c9090a81e
Filip <fivanusec@gmail.com>
2021-05-03 09:54:21 +0200

    Update _document.tsx

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
-import AdSense from "react-adsense";
 import { AdBanner } from "../components/AdBanner";
 
 export default class Document extends NextDocument {
