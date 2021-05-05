d5fe1b4d6dd31b85594a4f85c275ef743a6be81d
Filip <fivanusec@gmail.com>
2021-05-05 11:40:58 +0200

    router change in useIsAuth.ts

--- web/src/utils/useIsAuth.ts
+++ web/src/utils/useIsAuth.ts
@@ -1,13 +1,12 @@
 import { useMeQuery } from "../generated/graphql";
-import { useRouter } from "next/router";
+import Router from "next/router";
 import { useEffect } from "react";
 
 export const useIsAuth = () => {
-  const router = useRouter();
   const [{ data, fetching }] = useMeQuery();
   useEffect(() => {
     if (!fetching && !data?.me) {
-      router.replace("/login?next=" + router.pathname);
+      Router.replace("/login?next=" + Router.pathname);
     }
-  }, [fetching, data, router]);
+  }, [fetching, data, Router]);
 };
