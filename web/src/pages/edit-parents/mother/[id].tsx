2a046b5a6da71d70d858afa1d94e0e4abef8a57d
Filip Ivanušec <fivanusec@gmail.com>
2021-08-01 15:38:31 +0200

    Refactor edit parents

--- web/src/pages/edit-parents/mother/[id].tsx
+++ web/src/pages/edit-parents/mother/[id].tsx
@@ -2,32 +2,135 @@ import React from "react";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
 import { withUrqlClient } from "next-urql";
 import {
+  Exact,
+  FindMotherQuery,
+  ParentsInput,
+  UpdateMotherMutation,
   useFindMotherQuery,
   useUpdateMotherMutation,
 } from "../../../generated/graphql";
 import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
   Box,
   Button,
   Flex,
   Heading,
   HStack,
   IconButton,
-  Spinner,
   Stack,
   useToast,
+  UseToastOptions,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useGetId } from "../../../utils/getID";
-import { Layout } from "../../../components/Layout";
 import { Formik, Form } from "formik";
-import { InputField } from "../../../components/InputField";
-import { useTranslation } from "react-i18next";
+import { useTranslation, TFunction } from "react-i18next";
 import { useIsAuth } from "../../../utils/useIsAuth";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import { OperationContext, OperationResult } from "urql";
+import { NextRouter, useRouter } from "next/router";
+import { useGetId } from "../../../utils/getID";
+import { Layout } from "../../../components/Layout";
+import { InputField } from "../../../components/InputField";
+import { CustomAlert } from "../../../components/Alerts";
+import { CustomSpinner } from "../../../components/Spinner";
+
+const EditMotherForm = (
+  data: FindMotherQuery,
+  updateMother: (
+    variables?: Exact<{
+      motherId: number;
+      options: ParentsInput;
+    }>,
+    context?: Partial<OperationContext>
+  ) => Promise<
+    OperationResult<
+      UpdateMotherMutation,
+      Exact<{
+        motherId: number;
+        options: ParentsInput;
+      }>
+    >
+  >,
+  toast: (options?: UseToastOptions) => string | number,
+  router: NextRouter,
+  t: TFunction<"data">
+) => (
+  <Formik
+    initialValues={{
+      name: data?.findMother.Name,
+      surname: data?.findMother.Surname,
+      email: data?.findMother.Email,
+      phone: data?.findMother.Phone,
+    }}
+    onSubmit={async (values) => {
+      const { error } = await updateMother({
+        motherId: data.findMother.Id,
+        options: {
+          name: values.name,
+          surname: values.surname,
+          email: values.email,
+          // @ts-expect-error
+          phone: parseInt(values.phone),
+        },
+      });
+      if (!error) {
+        toast({
+          title: t("edit-mother.toast.title"),
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+        router.back();
+      }
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form>
+        <Stack spacing={4} marginBottom="1rem">
+          <InputField
+            name="name"
+            placeholder={t("edit-mother.form.placeholders.name")}
+            label={t("edit-mother.form.name")}
+            type="text"
+            required
+          />
+          <InputField
+            name="surname"
+            placeholder={t("edit-mother.form.placeholders.surname")}
+            label={t("edit-mother.form.surname")}
+            type="text"
+            required
+          />
+          <InputField
+            name="email"
+            placeholder={t("edit-mother.form.placeholders.email")}
+            label={t("edit-mother.form.email")}
+            type="email"
+            required
+          />
+          <InputField
+            name="phone"
+            placeholder={t("edit-mother.form.placeholders.phone")}
+            label={t("edit-mother.form.phone")}
+            type="text"
+            required
+          />
+          <Button
+            bg="blue.400"
+            colorScheme="navItem"
+            borderRadius="12px"
+            py="4"
+            px="4"
+            lineHeight="1"
+            size="md"
+            isLoading={isSubmitting}
+            type="submit"
+          >
+            {t("edit-mother.form.btn")}
+          </Button>
+        </Stack>
+      </Form>
+    )}
+  </Formik>
+);
 
 const EditMother = ({}) => {
   const { t } = useTranslation("data", { useSuspense: false });
@@ -42,57 +145,19 @@ const EditMother = ({}) => {
     },
   });
   const [, updateMother] = useUpdateMotherMutation();
+
   if (fetching) {
-    return (
-      <Flex
-        p={200}
-        minHeight="100%"
-        width="100%"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-          minH="250px"
-          minW="250px"
-        />
-      </Flex>
-    );
+    return <CustomSpinner />;
   } else if (!data?.findMother) {
     return (
-      <Flex
-        p={200}
-        minHeight="100%"
-        width="100%"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <Alert
-          status="error"
-          variant="subtle"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          height="200px"
-        >
-          <AlertIcon boxSize="40px" mr={0} />
-          <AlertTitle mt={4} mb={1} fontSize="lg">
-            {t("edit-mother.alert.title")}
-          </AlertTitle>
-          <AlertDescription maxWidth="sm">
-            {t("edit-mother.alert.desc")}
-          </AlertDescription>
-        </Alert>
-      </Flex>
+      <CustomAlert
+        name={t("edit-father.alert.title")}
+        status="error"
+        data={t("edit-father.alert.desc")}
+      />
     );
   }
+
   return (
     <Layout variant={"column"} navbarVariant="user">
       <title>{t("edit-mother.main-header")}</title>
@@ -127,79 +192,7 @@ const EditMother = ({}) => {
         mt={10}
       >
         <Box width={{ base: "90%", md: "400px" }} rounded="lg">
-          <Formik
-            initialValues={{
-              name: data?.findMother.Name,
-              surname: data?.findMother.Surname,
-              email: data?.findMother.Email,
-              phone: data?.findMother.Phone,
-            }}
-            onSubmit={async (values) => {
-              const { error } = await updateMother({
-                motherId: data.findMother.Id,
-                options: {
-                  ...values,
-                },
-              });
-              if (!error) {
-                toast({
-                  title: t("edit-mother.toast.title"),
-                  status: "success",
-                  duration: 9000,
-                  isClosable: true,
-                });
-                router.back();
-              }
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <Stack spacing={4} marginBottom="1rem">
-                  <InputField
-                    name="name"
-                    placeholder={t("edit-mother.form.placeholders.name")}
-                    label={t("edit-mother.form.name")}
-                    type="text"
-                    required
-                  />
-                  <InputField
-                    name="surname"
-                    placeholder={t("edit-mother.form.placeholders.surname")}
-                    label={t("edit-mother.form.surname")}
-                    type="text"
-                    required
-                  />
-                  <InputField
-                    name="email"
-                    placeholder={t("edit-mother.form.placeholders.email")}
-                    label={t("edit-mother.form.email")}
-                    type="email"
-                    required
-                  />
-                  <InputField
-                    name="phone"
-                    placeholder={t("edit-mother.form.placeholders.phone")}
-                    label={t("edit-mother.form.phone")}
-                    type="text"
-                    required
-                  />
-                  <Button
-                    bg="blue.400"
-                    colorScheme="navItem"
-                    borderRadius="12px"
-                    py="4"
-                    px="4"
-                    lineHeight="1"
-                    size="md"
-                    isLoading={isSubmitting}
-                    type="submit"
-                  >
-                    {t("edit-mother.form.btn")}
-                  </Button>
-                </Stack>
-              </Form>
-            )}
-          </Formik>
+          {EditMotherForm(data, updateMother, toast, router, t)}
         </Box>
       </Flex>
     </Layout>
