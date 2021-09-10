9bd0f41b1410df484f8887293c5a73d5579fb1af
Filip Ivanušec <fivanusec@gmail.com>
2021-09-10 08:25:28 +0200

    Fix account verify bug

--- web/src/pages/verify-account/[token].tsx
+++ web/src/pages/verify-account/[token].tsx
@@ -40,87 +40,96 @@ const VerifyAccountForm = (
         token: string;
       }>
     >
-  >,
-  handleClick: () => void,
-  checked: boolean
-) => (
-  <Formik
-    initialValues={{
-      token: typeof router.query.token === "string" ? router.query.token : "",
-    }}
-    onSubmit={async (values, { setErrors }) => {
-      const response = await verifyAccount({
-        token: typeof router.query.token === "string" ? router.query.token : "",
-      });
-      if (response.data?.verifyAccount.errors) {
-        toast({
-          title: response.data?.verifyAccount.errors[0].field.toLocaleUpperCase(),
-          description: response.data?.verifyAccount.errors[0].message,
-          status: "error",
-          duration: 9000,
-          isClosable: true,
-        });
-      } else {
-        toast({
-          title: t("verify-account.toast.title"),
-          status: "success",
-          duration: 9000,
-          isClosable: true,
-        });
-        router.push("/");
-      }
-    }}
   >
-    {({ isSubmitting }) => (
-      <Form>
-        <Stack spacing={4} marginBottom="1rem">
-          <Checkbox
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            {t("verify-account.privacy")}
-          </Checkbox>
-          <Stack marginBottom="1rem">
-            <Button
-              isDisabled={!checked}
-              bg="blue.400"
-              colorScheme="navItem"
-              borderRadius="12px"
-              py="4"
-              px="4"
-              lineHeight="1"
-              size="md"
-              isLoading={isSubmitting}
-              type="submit"
+) => {
+  const [checked, setChecked] = useState(false);
+  const handleClick = () => setChecked(!checked);
+  console.log(checked);
+  return (
+    <Formik
+      initialValues={{
+        token: typeof router.query.token === "string" ? router.query.token : "",
+      }}
+      onSubmit={async (values, { setErrors }) => {
+        const response = await verifyAccount({
+          token:
+            typeof router.query.token === "string" ? router.query.token : "",
+        });
+        if (response.data?.verifyAccount.errors) {
+          toast({
+            title:
+              response.data?.verifyAccount.errors[0].field.toLocaleUpperCase(),
+            description: response.data?.verifyAccount.errors[0].message,
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          });
+        } else {
+          toast({
+            title: t("verify-account.toast.title"),
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+          router.push("/");
+        }
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Stack spacing={4} marginBottom="1rem">
+            <Checkbox
+              onChange={() => {
+                handleClick();
+              }}
             >
-              {t("verify-account.btn")}
-            </Button>
+              {t("verify-account.privacy")}
+            </Checkbox>
+            <Stack marginBottom="1rem">
+              <Button
+                isDisabled={!checked}
+                bg="blue.400"
+                colorScheme="navItem"
+                borderRadius="12px"
+                py="4"
+                px="4"
+                lineHeight="1"
+                size="md"
+                isLoading={isSubmitting}
+                type="submit"
+              >
+                {t("verify-account.btn")}
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
-      </Form>
-    )}
-  </Formik>
-);
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 const VerifyAccount: NextPage = ({}) => {
   const { t } = useTranslation("data", { useSuspense: false });
-  const [checked, setChecked] = useState(false);
   const [, verifyAccount] = useVerifyAccountMutation();
   const router = useRouter();
   const toast = useToast();
-  const handleClick = () => {
-    setChecked(true);
-  };
 
   return (
     <Flex
-      p={200}
+      p={[0, 0, 0, 200, 200]}
+      alignItems="center"
       minHeight="100%"
       width="100%"
-      alignItems="center"
       justifyContent="center"
+      justifySelf="center"
+      justify={{
+        s: "center",
+        md: "center",
+        xl: "center",
+      }}
       flexDirection="column"
+      mt={["-0", "-0", "-0", "auto", "auto"]}
+      mb={["-0", "-0", "-0", "auto", "auto"]}
     >
       <Box
         width={{ base: "90%", md: "400px" }}
@@ -133,14 +142,7 @@ const VerifyAccount: NextPage = ({}) => {
         <Heading color={"blue.400"} marginBottom="1.5rem">
           {t("verify-account.main-header")}
         </Heading>
-        {VerifyAccountForm(
-          router,
-          toast,
-          t,
-          verifyAccount,
-          handleClick,
-          checked
-        )}
+        {VerifyAccountForm(router, toast, t, verifyAccount)}
         <Divider marginBottom="1rem" />
       </Box>
       <Footer variant="small" />
