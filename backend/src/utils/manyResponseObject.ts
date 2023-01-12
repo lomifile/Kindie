735bdbd8c68ec48f46bb0d543a07810437cf28e5
Filip Ivanušec <filip.ivanusec@chanalyze.com>
2023-01-12 09:23:12 +0100

    Refactor Mother and father resolvers

---
+++ backend/src/utils/manyResponseObject.ts
@@ -0,0 +1,14 @@
+import { ClassType, Field, ObjectType } from "type-graphql";
+import { FieldError } from "./Errors";
+
+export default function ManyResponse<TEntity>(TEntity: ClassType<TEntity>) {
+	@ObjectType({ isAbstract: true })
+	abstract class ManyResponseObject {
+		@Field(() => [TEntity], { nullable: true })
+		data?: TEntity[];
+
+		@Field(() => [FieldError], { nullable: true })
+		errors?: FieldError[];
+	}
+	return ManyResponseObject;
+}
